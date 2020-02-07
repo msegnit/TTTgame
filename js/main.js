3c6bde4c549cc512fd6073a53f266c57dcfdedc4
Melony Segnit <melonysegnit@Melonys-MacBook-Air.local>
2020-02-06 18:23:43 -0600

    new TTT

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 /*----- app's state (variables) -----*/
 
 let board, turn, winner;
-var confettiSettings = { target: 'my-canvas' };
 
 /*----- cached element references -----*/
 
 var sq = document.querySelectorAll('td div');
-var message = document.querySelector('h1');
+var message = document.querySelector('h2');
 
 /*----- event listeners -----*/
 
 document.querySelector('.board').addEventListener('click', handleClick);
-document.querySelector('button').addEventListener('click', initialize);
+document.querySelector('.button').addEventListener('click', initialize);
 
 /*----- functions -----*/
 
@@ -19,36 +18,49 @@ function initialize() {
    board = [null, null, null, null, null, null, null, null, null];
    turn = 1;
    winner = null;
-   render();
-    
+   message.innerHTML = "It's X's Turn!";
     };
 
-   // function checkForWin() {
-    //     if() {
-   //  board = [0] + [1] + [2] === 3 ||
-   //  board = [3] + [4] + [5] === 3 ||
-   //  board = [6] + [7] + [8] === 3 ||
-   //  board = [0] + [3] + [6] === 3 ||
-   //  board = [1] + [4] + [7] === 3 ||
-   //  board = [2] + [5] + [8] === 3 ||
-   //  board = [0] + [4] + [8] === 3 ||
-   //  board = [2] + [4] + [6] === 3 ||     
-    //     }
-   // }
+   function checkForWin() {
+       console.log("test")
+        if( 
+    Math.abs(board[0] + board[1] + board[2]) === 3 ||
+    Math.abs(board[3] + board[4] + board[5]) === 3 ||
+    Math.abs(board[6] + board[7] + board[8]) === 3 ||
+    Math.abs(board[0] + board[3] + board[6]) === 3 ||
+    Math.abs(board[1] + board[4] + board[7]) === 3 ||
+    Math.abs(board[2] + board[5] + board[8]) === 3 ||
+    Math.abs(board[0] + board[4] + board[8]) === 3 ||
+    Math.abs(board[2] + board[4] + board[6]) === 3){
+        message.innerHTML = "There's A Winner!!";
+        console.log("test2")
+    }
+       
+    }
 
     function handleClick(evt) {
         let square = document.getElementById(evt.target.id)
-        //let id = (evt.target.id);
-        //id = parseInt(id[2]);
-        //board[id] = 4
-        //console.log(board)
+        let id = (evt.target.id);
+        id = parseInt(id[2]);
+        board[id] = turn
+        console.log(board)
     
         if(turn ===1) {
             square.innerHTML="x"; 
-            turn =-1
+            message.innerHTML = "It's O's Turn!";
+             checkForWin()
+             turn =-1
         } else {
             square.innerHTML="o"
+            message.innerHTML = "It's X's Turn!";
+             checkForWin()
             turn =1
         }
         console.log(square);
     }
+
+    function init(button) {
+        initialize()   
+    }
+
+    initialize()
